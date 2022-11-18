bcbbf66f9f6402126ec2f8074fe09d117f33f7db
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-11-18 17:24:52 +0100

    Fix insert method

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ class Tree {
 
         let node = new Node(array[mid])
 
-        console.log(array.slice(start, mid))
+        // console.log(array.slice(start, mid))
 
         node.left = this.buildTree(array.slice(start, mid))
         node.right = this.buildTree(array.slice(mid + 1))
@@ -34,23 +34,23 @@ class Tree {
         if (pointer === null) {
             return new Node(value);
         }
-        if (pointer.value === value) {
+        if (pointer.data === value) {
             return;
         } 
 
-        if (pointer.value < value) {
-            pointer.left = this.insert(value, pointer.left)
-        } else {
+        if (pointer.data < value) {
             pointer.right = this.insert(value, pointer.right)
+        } else {
+            pointer.left = this.insert(value, pointer.left)
         }
         return pointer
 
     }
 }
 
-let tree = new Tree([1,2,3,4,5,6,7])
+let tree = new Tree([1,2,3,5,6,7,8])
 
-console.log(tree.buildTree([1,2,3,4,5,6,7]))
+// console.log(tree.buildTree([1,2,3,4,5,6,7]))
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
@@ -62,5 +62,13 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
   }
 
+const util = require("util")
+
+// prettyPrint(tree.buildTree([1,2,3,4,5,6,7]))
+// tree.insert(8)
+// console.log(util.inspect(tree.insert(4), false, null, true /* enable colors */))
+tree.insert(4)
+
+prettyPrint(tree.root)
 
 
